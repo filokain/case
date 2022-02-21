4935d6456689d54ee9ec875a6613f621c88aa1c1
filokain <99543638+filokain@users.noreply.github.com>
2022-02-21 11:13:20 +0300

    Update page.php

--- page.php
+++ page.php
@@ -14,8 +14,7 @@ $query = mysqli_query($conn, "SELECT * FROM $dbarticles WHERE id_teacher='$page'
   <meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-<!-- начало -->
-  <title>Об учителе</title>
+<title>Об учителе</title>
 </head>
 <body>
 <?php
