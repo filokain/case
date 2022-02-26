d2d6a170d7ba1a8583f7ee0446138091737de5a6
filokain <99543638+filokain@users.noreply.github.com>
2022-02-26 13:00:06 +0300

    Update page.php

--- page.php
+++ page.php
@@ -20,7 +20,20 @@ $query = mysqli_query($conn, "SELECT * FROM $dbarticles WHERE id_teacher='$page'
 <?php
 $article = mysqli_fetch_assoc($query);
 ?>
-
+<div class="container mt-5" align="center">
+<div class="d-flex flex-wrap">
+<div class="card mb-5 shadow-sm">
+  <div class="card-header">
+    <h2 class="mb-5"><?echo $article['teacher_name'];?></h2>
+  </div>
+  <div class="card-body">
+    <img src="<?php echo $article['pic']; ?>." class="img-thumbnail" width = "50%">
+  <ul class="list-unstyled mt-3 mb-4">
+    <li><p style="color: gold; font-size: 250%;"><? echo $article['teacher_rating']?></p></li>
+  <li style="text-align: left;"><?echo $article['teacher_content'];?></li>
+  </ul>
+  <a href="teachers.php"><button type="button" class="btn btn-lg btn-block">Назад</button></a>
+  </div>
 </div>
 </div>
 </div>
