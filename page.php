c7611db87a8e78729360859e5ff156b92a14b6bd
filokain <99543638+filokain@users.noreply.github.com>
2022-03-03 11:37:19 +0300

    Update page.php

---
+++ page.php
@@ -0,0 +1,41 @@
+<?php
+require_once("connect.php");
+if($conn == false){
+	echo "Досведанья, мистер лох!";
+	echo mysqli_connect_errno();
+	exit();
+}
+$page = $_GET['id'];
+$query = mysqli_query($conn, "SELECT * FROM $dbarticles WHERE id_teacher='$page' ");
+?>
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<title>Информация об учителях</title>
+</head>
+<body>
+<?php
+$article = mysqli_fetch_assoc($query);
+?>
+<div class="container mt-5" align="center">
+<div class="d-flex flex-wrap">
+<div class="card mb-5 shadow-sm">
+  <div class="card-header">
+    <h2 class="mb-5"><?echo $article['teacher_name'];?></h2>
+  </div>
+  <div class="card-body">
+    <img src="<?php echo $article['pic']; ?>." class="img-thumbnail" width = "50%">
+  <ul class="list-unstyled mt-3 mb-4">
+    <li><p style="color: gold; font-size: 250%;"><? echo $article['teacher_rating']?></p></li>
+  <li style="text-align: left;"><?echo $article['teacher_content'];?></li>
+  </ul>
+  <a href="teachers.php"><button type="button" class="btn btn-lg btn-block">Назад</button></a>
+  </div>
+</div>
+</div>
+</div>
+</body>
+</html>
